547ae56bbd9df1934a6c41b28820225c248b25d5
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-02-06 12:39:50 +0900

    [Bronze II] Title: Hashing, Time: 208 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,19 +1,18 @@
 import java.util.*;
 
 public class Main {
-    static int M = 1234567891;
-    static int R = 31;
-    static int H = 0;
+    static long M = 1234567891L;
+    static long R = 1L;
+    static long H = 0L;
 
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int L = sc.nextInt();
-            int[] intArray = Arrays.stream(sc.next().split(""))
-                    .mapToInt((String s) -> (int)s.charAt(0) - 96)
-                    .toArray();
+            String str = sc.next();
 
             for (int i = 0; i < L; i++) {
-                H += ( (Math.pow(R, i) % M)  * intArray[i]) % M;
+                H += (((int) str.charAt(i) - 96) * R) % M;
+                R = (R * 31) % M;
             }
 
             System.out.println(H % M);

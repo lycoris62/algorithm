074e811f65159a5e3a7a8ddb8d38c487b32d2df4
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-02-06 13:41:51 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2768 ms, Memory: 385928 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        br.close();
+        arr = Arrays.stream(arr).sorted().toArray();
+        for (int i : arr) {
+            bw.write(i + "\n");
+        }
+        bw.close();
+    }
+}

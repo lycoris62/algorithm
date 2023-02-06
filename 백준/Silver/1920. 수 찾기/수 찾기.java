79c11afc53b22ff830662e9129c1e4ffb37fb3fa
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-02-06 15:41:39 +0900

    [Silver IV] Title: 수 찾기, Time: 716 ms, Memory: 63136 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for (int ele : input) {
+            map.put(ele, 1);
+        }
+        int M = Integer.parseInt(br.readLine());
+        int[] output = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for (int ele : output) {
+            bw.write(map.getOrDefault(ele, 0) + "\n");
+        }
+        bw.close();
+    }
+}

e57e8b8e0fcf9dd401e6587639c2813d43d8d40b
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-02-13 23:23:03 +0900

    [Bronze II] Title: A+B - 6, Time: 136 ms, Memory: 15800 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), ",");
+            bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "\n");
+        }
+        bw.close();
+    }
+}

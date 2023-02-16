727628e1d4c56f2d7f819ddefdda825d089125b4
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-02-16 23:46:09 +0900

    [Silver II] Title: 랜선 자르기, Time: 464 ms, Memory: 30780 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+ 
+		int K = in.nextInt();
+		int N = in.nextInt();
+ 
+		int[] arr = new int[K];
+		
+		long max = 0;
+		for (int i = 0; i < K; i++) {
+			arr[i] = in.nextInt();
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		max++; 
+ 
+		long min = 0; 
+		long mid = 0; 
+ 
+		while (min < max) { 
+			mid = (max + min) / 2;
+			long count = 0;
+			for (int i = 0; i < arr.length; i++) {
+				count += (arr[i] / mid);
+			}
+			if(count < N) {
+				max = mid;
+			}
+			else {
+				min = mid + 1;
+			}
+		}
+		System.out.println(min - 1);
+	}
+}

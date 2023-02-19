ca2f5ca69df1f761622c39065d25b5dbd336c890
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-02-19 13:51:47 +0900

    [Gold V] Title: 동전 1, Time: 144 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -8,9 +8,8 @@ public class Main {
 
     static int N;
     static int K;
-    static List<Integer> list;
-    static int[][] dp;
-    static int[] dpp;
+    static List<Integer> coins;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,45 +17,18 @@ public class Main {
 
         N = stoi(st.nextToken());
         K = stoi(st.nextToken());
-        list = new ArrayList<>(N);
-        dp = new int[N][K + 1];
-        dpp = new int[K + 1];
+        coins = new ArrayList<>(N);
+        dp = new int[K + 1];
+        dp[0] = 1;
 
-//        int i = N;
-//        while (i-- > 0) list.add(stoi(br.readLine()));
-        for (int i = 0; i < N; i++) list.add(stoi(br.readLine()));
+        for (int i = 0; i < N; i++) coins.add(stoi(br.readLine()));
 
-//        for (int i = 0; i <= K; i++) {
-//            dp[0][i] = 1;
-//        }
-//        for (int j = 1; j < N; j++) {
-//            for (int k = 0; k <= K; k++) {
-//                if (k >= list.get(j))
-//                    dp[j][k] = dp[j - 1][k] + dp[j][k - list.get(j)];
-//                else
-//                    dp[j][k] = dp[j - 1][k];
-//            }
-//        }
-//        for (int[] ints : dp) {
-//            for (int anInt : ints) {
-//                System.out.print(anInt + " ");
-//            }
-//            System.out.println();
-//        }
-//
-//        System.out.println(dp[N - 1][K]);
-
-
-
-        dpp[0] = 1;
         for (int i = 0; i < N; i++) {
-            for (int j = list.get(i); j <= K; j++) {
-                dpp[j] += dpp[j - list.get(i)];
+            for (int j = coins.get(i); j <= K; j++) {
+                dp[j] += dp[j - coins.get(i)];
             }
         }
-//        for (int i : dpp) {
-//            System.out.print(i + " ");
-//        }
-        System.out.println(dpp[K]);
+
+        System.out.println(dp[K]);
     }
 }

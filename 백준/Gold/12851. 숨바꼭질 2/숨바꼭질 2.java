ae7e38be8097796423d78a5bab760accbefaa4d3
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-02-19 18:09:09 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 168 ms, Memory: 21344 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -1,19 +1,27 @@
-import java.util.*;
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-class Main {
-    static int N, K;
-    static int[] time = new int[100001];
-    static int minTime = 987654321;
+public class Main {
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int N;
+    static int K;
+    static Queue<Integer> q;
+    static int[] visited = new int[100001];
+    static int minSecond = 987654321;
     static int count = 0;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        N = stoi(st.nextToken());
+        K = stoi(st.nextToken());
+        q = new LinkedList<>();
 
         if (N >= K) {
             System.out.println((N-K) + "\n1");
@@ -22,43 +30,39 @@ class Main {
 
         bfs();
 
-        System.out.println(minTime + "\n" + count);
+        System.out.println(minSecond);
+        System.out.println(count);
     }
 
     static void bfs() {
-        Queue<Integer> q = new LinkedList<Integer>();
-
         q.add(N);
-        time[N] = 1;
+        visited[N] = 1;
 
         while (!q.isEmpty()) {
             int now = q.poll();
 
-            // now 방문 시간이 최소 시간보다 크면 뒤는 더 볼 필요 없음
-            if (minTime < time[now]) return;
+            if (minSecond < visited[now]) return;
 
-            for (int i=0; i<3; i++) {
+            for (int i = 0; i < 3; i++) {
                 int next;
 
-                if (i == 0)         next = now + 1;
-                else if (i == 1)    next = now - 1;
-                else                next = now * 2;
+                if (i == 0) next = now - 1;
+                else if (i == 1) next = now + 1;
+                else next = now * 2;
 
-                if (next < 0 || next > 100000) continue;
+                if (next < 0 || 100_000 < next) continue;
 
                 if (next == K) {
-                    minTime = time[now];
+                    minSecond = visited[now];
                     count++;
                 }
 
-                // 첫 방문이거나 (time[next] == 0)
-                // 이미 방문한 곳이어도 같은 시간에 방문했다면 (time[next] == time[now] + 1)
-                // 경우의 수에 추가될 수 있기 때문에 Queue 에 한번 더 넣어줌
-                if (time[next] == 0 || time[next] == time[now] + 1) {
+                if (visited[next] == 0 || visited[next] == visited[now] + 1) {
                     q.add(next);
-                    time[next] = time[now] + 1;
+                    visited[next] = visited[now] + 1;
                 }
             }
+
         }
     }
 }

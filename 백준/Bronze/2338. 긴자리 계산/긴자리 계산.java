23e53f6adfbe0c6a31553a1b6cd70622b0775aac
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-02-26 12:03:55 +0900

    [Bronze V] Title: 긴자리 계산, Time: 144 ms, Memory: 14612 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BigInteger n = new BigInteger(br.readLine());
+        BigInteger m = new BigInteger(br.readLine());
+
+        System.out.println(n.add(m));
+        System.out.println(n.subtract(m));
+        System.out.println(n.multiply(m));
+    }
+}

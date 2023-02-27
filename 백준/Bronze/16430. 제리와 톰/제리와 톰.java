33070de417c36e89c5941fd8645d3d62dad508d2
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-02-27 23:53:41 +0900

    [Bronze V] Title: 제리와 톰, Time: 132 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int N;
+    static int M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = stoi(st.nextToken());
+        M = stoi(st.nextToken());
+
+        System.out.printf("%d %d", M - N, M);
+    }
+}

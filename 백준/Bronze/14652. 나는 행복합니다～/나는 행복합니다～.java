4d6afda7498103afb2900897c8a03edbb65a8cb1
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-02-27 13:21:00 +0900

    [Bronze V] Title: 나는 행복합니다~, Time: 128 ms, Memory: 14424 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int N;
+    static int M;
+    static int K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = stoi(st.nextToken());
+        M = stoi(st.nextToken());
+        K = stoi(st.nextToken());
+
+        int n = K / M;
+        int m = K % M;
+        
+        System.out.printf("%d %d", n, m);
+    }
+}

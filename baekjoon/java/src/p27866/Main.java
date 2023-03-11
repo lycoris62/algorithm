3891363b66f518c1a48616b2c86b7601763332b6
Lycoris62 <wodbs7758@naver.com>
2023-03-11 14:22:53 +0900

    [백준 27866] Bronze 5

---
+++ baekjoon/java/src/p27866/Main.java
@@ -0,0 +1,16 @@
+package p27866;
+
+import java.io.*;
+
+public class Main {
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str.charAt(stoi(br.readLine()) - 1));
+    }
+}
+

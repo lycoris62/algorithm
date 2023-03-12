2bce0d93ad1adcc75c8e0c6fa4e2337763a85ec7
Lycoris62 <wodbs7758@naver.com>
2023-03-12 21:30:30 +0900

    [백준 13277] Bronze 5

---
+++ baekjoon/java/src/p13277/Main.java
@@ -0,0 +1,17 @@
+package p13277;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+
+        System.out.println(a.multiply(b));
+    }
+}

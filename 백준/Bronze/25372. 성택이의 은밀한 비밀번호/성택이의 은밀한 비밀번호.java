41445894668cc944b4b0a14f2c8c0408500ca5c2
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-03-21 18:20:47 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 172 ms, Memory: 14744 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = stoi(st.nextToken());
+        while (N-->0) {
+            String str = br.readLine();
+            System.out.println(6 <= str.length() && str.length() <= 9 ? "yes" : "no");
+        }
+    }
+}

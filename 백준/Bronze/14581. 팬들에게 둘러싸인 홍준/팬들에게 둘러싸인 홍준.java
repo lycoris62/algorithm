107470066e81b532fd3e35f6adc7701a46a85225
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-03-23 12:21:22 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 148 ms, Memory: 15912 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + str + "::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}

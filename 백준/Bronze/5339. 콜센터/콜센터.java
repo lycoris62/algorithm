cb2efffc2a470e6deb7f97305ef7de519e03f7c7
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-04-03 09:15:37 +0900

    [Bronze V] Title: 콜센터, Time: 112 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //백준 5339 번 문제 풀어줘
+
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+    }
+}

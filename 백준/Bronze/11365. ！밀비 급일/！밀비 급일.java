6e78b995ea6c143103dc16dad8dc9d9c0fe1ee89
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-04-27 14:32:45 +0900

    [Bronze IV] Title: !밀비 급일, Time: 116 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String s = br.readLine();
+            if (s.equals("END")) break;
+            System.out.println(new StringBuilder(s).reverse());
+        }
+    }
+}

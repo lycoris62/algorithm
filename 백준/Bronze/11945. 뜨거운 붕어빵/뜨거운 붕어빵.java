0d59123a3cc1277178654c83d622fa6a0afc5133
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-05-03 20:31:41 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = stoi(st.nextToken());
+        M = stoi(st.nextToken());
+
+        while (N-->0) {
+            System.out.println(new StringBuilder(br.readLine()).reverse());
+        }
+    }
+}

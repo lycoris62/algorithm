837460aa8eeb2b7de6ff969657ed3e1be1b48443
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-06-27 23:36:47 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 260 ms, Memory: 15472 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = stoi(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < N; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

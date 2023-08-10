47dc119323e6fc1a92d419116c90d9947e291e34
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-08-10 16:46:38 +0900

    [Silver III] Title: N과 M (1), Time: 256 ms, Memory: 22836 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,14 +1,13 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N, M;
     static StringBuilder sb = new StringBuilder();
+    static boolean[] visited;
+    static int[] arr;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,31 +16,30 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= N; i++) {
-            List<Integer> list = new ArrayList<>();
-            list.add(i);
-            dfs(1, list);
-        }
+        visited = new boolean[N];
+        arr = new int[M];
+
+        bt(0);
 
         System.out.println(sb);
     }
 
-    static void dfs(int depth, List<Integer> list) {
+    static void bt(int depth) {
         if (depth == M) {
-            for (int x : list) {
+            for (int x : arr) {
                 sb.append(x).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = 1; i <= N; i++) {
-            if (list.contains(i)) {
-                continue;
-            }
-            List<Integer> li = new ArrayList<>(list);
-            li.add(i);
-            dfs(depth + 1, li);
+        for (int i = 0; i < N; i++) {
+            if (visited[i]) continue;
+
+            visited[i] = true;
+            arr[depth] = i + 1;
+            bt(depth + 1);
+            visited[i] = false;
         }
     }
 }

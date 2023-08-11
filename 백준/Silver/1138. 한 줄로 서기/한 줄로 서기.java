98feb73a2df03fc2b62a8c5e5bb18be1cc6ee885
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-08-11 15:15:24 +0900

    [Silver II] Title: 한 줄로 서기, Time: 212 ms, Memory: 17840 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -1,49 +1,21 @@
-
 import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
 
-    static int N;
-    static int[] arr, heights;
-    static boolean[] visited;
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        N = sc.nextInt();
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        IntStream.range(0, N).forEach(i -> arr[i] = sc.nextInt());
 
-        arr = new int[N];
-        heights = new int[N];
-        visited = new boolean[N];
-
-        IntStream.range(0, N).forEach(i -> heights[i] = sc.nextInt());
-        dfs(0);
-    }
-
-    static void dfs(int depth) {
-        if (depth == N) {
-            for (int i = 1; i <= N; i++) {
-                int cnt = 0;
-                for (int j = 0; j < N; j++) {
-                    if (i == arr[j]) break;
-                    if (arr[j] > i) cnt++;
-                }
-                if (heights[i - 1] != cnt) return;
-            }
-            
-            Arrays.stream(arr).forEach(x -> System.out.print(x + " "));
-            System.exit(0);
+        List<Integer> list = new ArrayList<>();
+        for (int i = N - 1; i >= 0; i--) {
+            list.add(arr[i], i + 1);
         }
 
-        for (int i = 0; i < N; i++) {
-            if (visited[i]) continue;
-
-            visited[i] = true;
-            arr[depth] = i + 1;
-            dfs(depth + 1);
-            visited[i] = false;
-        }
+        list.forEach(x -> System.out.print(x + " "));
     }
 }

2f19f3da4308b91c591a3b475db9c1cf75c4c3b7
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-08-11 13:34:04 +0900

    [Silver II] Title: N과 M (11), Time: 580 ms, Memory: 60668 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr, li;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[M];
+        li = new int[N];
+
+        IntStream.range(0, N).forEach(i -> li[i] = sc.nextInt());
+        Arrays.sort(li);
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            Arrays.stream(arr).forEach(i -> sb.append(i).append(" "));
+            sb.append("\n");
+            return;
+        }
+
+        int before = -1;
+        for (int i = 0; i < N; i++) {
+            if (before == li[i]) continue;
+
+            before = li[i];
+            arr[depth] = li[i];
+            dfs(depth + 1);
+        }
+    }
+}

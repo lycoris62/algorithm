b2be13278bcaeea45e960ab6d5e6467c0d52eed5
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-08-17 14:31:56 +0900

    [Silver I] Title: 행렬, Time: 252 ms, Memory: 18056 KB -BaekjoonHub

---
+++ 백준/Silver/1080. 행렬/행렬.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M, cnt = 0;
+    static int[][] mat, res;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(sc.nextLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        mat = new int[N][M];
+        res = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String str = sc.nextLine().replaceAll(" ", "");
+            for (int j = 0; j < M; j++) {
+                mat[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            String str = sc.nextLine().replaceAll(" ", "");
+            for (int j = 0; j < M; j++) {
+                res[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = 0; j < M - 2; j++) {
+                if (mat[i][j] != res[i][j]) {
+                    cnt++;
+                    change(i, j);
+                }
+            }
+        }
+
+        int flag = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (mat[i][j] == res[i][j]) {
+                    flag++;
+                }
+            }
+        }
+
+        if (flag == N * M) {
+            System.out.println(cnt);
+        } else {
+            System.out.println(-1);
+        }
+    }
+
+    static void change(int n, int m) {
+        for (int i = n; i < n + 3; i++) {
+            for (int j = m; j < m + 3; j++) {
+                mat[i][j] = mat[i][j] == 0 ? 1 : 0;
+            }
+        }
+    }
+}

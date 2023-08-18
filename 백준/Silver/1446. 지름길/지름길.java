5156eb6126c94ca282ce6daa8d1d0cf8b2392cb0
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-08-18 19:34:02 +0900

    [Silver I] Title: 지름길, Time: 224 ms, Memory: 17972 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.java
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -3,73 +3,52 @@ import java.util.*;
 public class Main {
 
     static int N, D;
-    static int[] dist;
-    static List<List<Node>> graph = new ArrayList<>();
+    static int[] dp;
     static final int MAX = 10_001;
+    static Map<Integer, List<Node>> map = new HashMap<>();
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
         N = sc.nextInt();
         D = sc.nextInt();
-        dist = new int[MAX];
-        Arrays.fill(dist, MAX);
-
-        for (int i = 0; i < MAX; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < MAX - 1; i++) {
-            graph.get(i).add(new Node(i + 1, 1));
-        }
+        dp = new int[MAX];
 
         for (int i = 0; i < N; i++) {
             int start = sc.nextInt();
             int end = sc.nextInt();
-            int cost = sc.nextInt();
+            int distance = sc.nextInt();
 
-            graph.get(start).add(new Node(end, cost));
+            if (!map.containsKey(end)) map.put(end, new ArrayList<>());
+            map.get(end).add(new Node(start, distance));
+
         }
-
-        dijkstra();
-
-        System.out.println(dist[D]);
+        
+        sol();
+        System.out.println(dp[D]);
     }
 
-    static void dijkstra() {
-        Queue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(0, 0));
-        dist[0] = 0;
+    static void sol() {
+        for (int i = 1; i < MAX; i++) {
+            int temp = dp[i - 1] + 1;
 
-        while (!pq.isEmpty()) {
-            Node nowNode = pq.poll();
-            int now = nowNode.index;
-            int distance = nowNode.cost;
-
-            if (dist[now] < distance) continue;
-
-            for (Node node : graph.get(now)) {
-                int cost = distance + node.cost;
-
-                if (cost < dist[node.index]) {
-                    dist[node.index] = cost;
-                    pq.add(new Node(node.index, cost));
+            if (map.containsKey(i)) {
+                for (Node node : map.get(i)) {
+                    temp = Math.min(temp, dp[node.index] + node.distance);
                 }
             }
+            
+            dp[i] = temp;
         }
     }
 
-    static class Node implements Comparable<Node> {
-        int index, cost;
+    static class Node {
+        int index, distance;
 
-        public Node(int index, int cost) {
+        public Node(int index, int distance) {
             this.index = index;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return Integer.compare(this.cost, o.cost);
+            this.distance = distance;
         }
     }
 }

9e495941972e6811d417b1aa15d91a9b7428771b
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-09-27 20:26:56 +0900

    [level 0] Title: 배열의 유사도, Time: 0.06 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -2,8 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] s1, String[] s2) {
-        Set<String> set = new HashSet<>();
-        for (String x : s1) set.add(x);
+        Set<String> set = new HashSet<>(List.of(s1));
         for (String x : s2) set.add(x);
         
         return s1.length + s2.length - set.size();

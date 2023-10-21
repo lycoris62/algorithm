040a476f0c77d2831bcf1debeaa484afdb515c9a
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-10-21 14:32:32 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 132 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int T, N;
+	static Map<String, List<String>> map;
+	static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        T = stoi(st.nextToken());
+
+		while (T-->0) {
+			N = stoi(br.readLine());
+			map = new HashMap<>();
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				String itemName = st.nextToken();
+				String type = st.nextToken();
+
+				if (!map.containsKey(type)) {
+					map.put(type, new ArrayList<>());
+				}
+
+				map.get(type).add(itemName);
+			}
+
+			int total = 1;
+			for (List<String> value : map.values()) {
+				total *= value.size() + 1;
+			}
+			total--;
+
+			sb.append(total).append("\n");
+		}
+
+		System.out.println(sb);
+    }
+}

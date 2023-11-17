c9a445039f65f6f7ed5574f1bc6f1dafde7cda3c
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-11-17 09:42:32 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 6.96 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,10 +1,7 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] seoul) {
-        for (int i = 0; i < seoul.length; i++) { 
-            if (seoul[i].contains("Kim")) {
-                return "김서방은 " + i + "에 있다";
-            }
-        }
-        return "";
+        return "김서방은 " + List.of(seoul).indexOf("Kim") + "에 있다";
     }
 }

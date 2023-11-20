0612f995c8f71ecf4d4036d2f11c346cb23aefaf
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-11-20 09:49:42 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,7 +1,5 @@
 class Solution {
     public String solution(String s) {
-        return s.length() % 2 == 0 
-            ? s.substring(s.length() / 2 - 1, s.length() / 2 + 1)
-            : s.substring(s.length() / 2, s.length() / 2 + 1);
+        return s.substring((s.length() - 1) / 2, s.length() / 2 + 1);
     }
 }

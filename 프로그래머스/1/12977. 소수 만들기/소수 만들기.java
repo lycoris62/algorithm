2e51cb9dce9658bc98782c68b4455672bba40ac9
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-12-13 19:46:07 +0900

    [level 1] Title: 소수 만들기, Time: 2.17 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,7 +1,7 @@
 class Solution {
+    
     int length = 1000 * 50;
     boolean[] composites = new boolean[length + 1];
-    int res = 0;
     
     private void setPrimes() {
         composites[0] = composites[1] = true;
@@ -18,37 +18,19 @@ class Solution {
     public int solution(int[] nums) {
         
         setPrimes();
-        boolean[] visited = new boolean[nums.length];
+        int answer = 0;
         
         for (int i = 0; i < nums.length - 2; i++) {
             for (int j = i + 1; j < nums.length - 1; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
                     int sum = nums[i] + nums[j] + nums[k];
                     if (!composites[sum]) {
-                        res++;
+                        answer++;
                     }
                 }
             }
         }
         
-        return res;
-    }
-    
-    private void dfs(int depth, int sum, int[] nums, boolean[] visited) {
-        if (depth == 3) {
-            if (!composites[sum]) {
-                composites[sum] = true;
-                res++;
-            }
-            return;
-        }
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                dfs(depth + 1, sum + nums[i], nums, visited);
-                visited[i] = false;
-            }
-        }
+        return answer;
     }
 }

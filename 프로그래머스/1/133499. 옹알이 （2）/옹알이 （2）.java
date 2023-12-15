921e21aeb414c09a7925e7e083e794f16e648085
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-12-15 10:21:41 +0900

    [level 1] Title: 옹알이 (2), Time: 1.03 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,25 +1,31 @@
 class Solution {
     
-    String[] availableWords = {"aya", "ye", "woo", "ma"};
+    String[] availableWords = { "aya", "ye", "woo", "ma" };
+    String[] canNotSpeakableWords = { "ayaaya", "yeye", "woowoo", "mama" };
     
     public int solution(String[] babbling) {
         int answer = 0;
+        
         for (String word : babbling) {
             if (isSpeakable(word)) {
                 answer++;
             }
         }
+        
         return answer;
     }
     
     private boolean isSpeakable(String word) {
-        if(word.contains("ayaaya") || word.contains("yeye") || word.contains("woowoo") || word.contains("mama")) {
-            return false;
+        for (String canNotSpeakableWord : canNotSpeakableWords) {
+            if (word.contains(canNotSpeakableWord)) {
+                return false;
+            }
         }
         
         for (String availableWord : availableWords) {
             word = word.replace(availableWord, " ");
         }
+        
         word = word.replace(" ", "");
         
         return word.length() == 0; 

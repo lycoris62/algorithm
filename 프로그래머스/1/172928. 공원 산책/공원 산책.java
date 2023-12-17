695c657cd321dcdc2273551f8c371248bef017d8
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-12-17 17:27:15 +0900

    [level 1] Title: 공원 산책, Time: 0.28 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Solution {
+    int N, M;
+    Map<String, int[]> dirMap = Map.of(
+    "E", new int[]{0, 1}, 
+    "W", new int[]{0, -1}, 
+    "S", new int[]{1, 0},
+    "N", new int[]{-1, 0});
+    
+    public int[] solution(String[] park, String[] routes) {
+        N = park.length;
+        M = park[0].length();
+        Node now = new Node();
+        
+        for (int i = 0; i < N; i++) {
+            String row = park[i];
+            for (int j = 0; j < M; j++) {
+                if (row.charAt(j) == 'S') {
+                    now.x = i;
+                    now.y = j;
+                    break;
+                }
+            }
+        }
+        
+        for (String comm : routes) {
+            String[] splits = comm.split(" ");
+            int[] dxy = dirMap.get(splits[0]);
+            int move = Integer.parseInt(splits[1]);
+            
+            Node next = new Node();
+            next.x = now.x;
+            next.y = now.y;
+            
+            for (int i = 0; i < move; i++) {
+                int nx = next.x + dxy[0];
+                int ny = next.y + dxy[1];
+                
+                if ((0 <= nx && nx < N) 
+                && (0 <= ny && ny < M) 
+                && (park[nx].charAt(ny) != 'X')) {
+                    next.x = nx;
+                    next.y = ny;
+                } else {
+                    next = now;
+                    break;
+                }
+            }
+            
+            now = next;
+        }
+        
+        return new int[] {now.x, now.y};
+    }
+    
+    class Node {
+        int x, y;
+    }
+}

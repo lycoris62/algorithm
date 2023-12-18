18128b36c7e0130503930ef8c507aa04d634f1cf
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-12-18 21:13:19 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,16 @@
+SELECT 
+    NAME, 
+    DATETIME
+FROM 
+    ANIMAL_INS
+WHERE 
+    ANIMAL_ID NOT IN (
+        SELECT 
+        DISTINCT 
+            ANIMAL_ID 
+        FROM 
+            ANIMAL_OUTS)
+ORDER BY 
+    DATETIME ASC
+LIMIT 
+    3

70236f32738a06524c31e2c69b352115da59a130
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-12-18 15:20:25 +0900

    [level 1] Title: 신고 결과 받기, Time: 3.10 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -6,7 +6,7 @@ class Solution {
     Map<String, List<String>> reportListMap = new HashMap<>();
     
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = {};
+        
         for (String user : id_list) {
             reportCountMap.put(user, 0);
             reportListMap.put(user, new ArrayList<>());
@@ -28,33 +28,11 @@ class Solution {
             reportCountMap.put(toUser, reportCountMap.get(toUser) + 1);
         }
         
-        // k번 신고 당한 유저는 1메일
-        // 신고 한 유저도 1메일 
-        
-        int[] result = new int[id_list.length];
-        
-        for (int i = 0; i < id_list.length; i++) {
-            String user = id_list[i];
-            // // 본인이 k번 신고 당한 경우 메일 +1
-            // if (reportCountMap.get(user) >= k) {
-            //     result[i]++;
-            // }
-            // 본인이 신고한 유저들의 신고 횟수가 k번 이상인 경우 그 경우당 메일 +1 
-            List<String> reportList = reportListMap.get(user);
-            for (String toUser : reportList) {
-                if (reportCountMap.get(toUser) >= k) {
-                    result[i]++;
-                }
-            }
-        }
-        
-        
-        
-        // return Arrays.stream(id_list)
-        //     .mapToInt(toUser -> reportCountMap.get(toUser))
-        //     .toArray();
-        
-        // return answer;
-        return result;
+        return Arrays.stream(id_list)
+            .mapToInt(fromUser -> (int) reportListMap.get(fromUser)
+                    .stream()
+                    .filter(toUser -> reportCountMap.get(toUser) >= k)
+                    .count())
+            .toArray();
     }
 }

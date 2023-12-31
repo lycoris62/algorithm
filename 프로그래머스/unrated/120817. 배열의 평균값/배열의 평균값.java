13e78e1c093b3a5141646693a18b26399ee6ba30
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-12-31 19:52:35 +0900

    [level unrated] Title: 배열의 평균값, Time: 4.59 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.java
@@ -2,12 +2,6 @@ import java.util.*;
 
 class Solution {
     public double solution(int[] numbers) {
-        int sum = 0;
-        
-        for (int num : numbers) {
-            sum += num;
-        }
-        
-        return (double) sum / numbers.length;
+        return Arrays.stream(numbers).average().orElse(0);
     }
 }

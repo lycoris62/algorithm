c513d268eaa38b0d6db8de049d09252919f0fcda
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-12-31 21:31:25 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 1.10 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -2,12 +2,8 @@ import java.util.*;
 
 class Solution {
     public long solution(long n) {
-        String[] list = String.valueOf(n).split("");
-        Arrays.sort(list);
-
         StringBuilder sb = new StringBuilder();
-        for (String aList : list) 
-            sb.append(aList);
+        Arrays.stream(String.valueOf(n).split("")).sorted().forEach(e -> sb.append(e));
 
         return Long.parseLong(sb.reverse().toString());
     }

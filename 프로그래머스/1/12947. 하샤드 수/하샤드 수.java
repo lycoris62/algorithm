ccc0c877ac9409a94736def6c35c77a0b9004af1
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-12-31 21:33:39 +0900

    [level 1] Title: 하샤드 수, Time: 1.63 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean solution(int x) {
-        int sum = String.valueOf(x).chars().map(ch -> ch - '0').sum();
-        return x % sum == 0;
+        return x % String.valueOf(x).chars().map(Character::getNumericValue).sum() == 0;
     }
 }

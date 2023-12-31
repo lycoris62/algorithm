baac01e593b461488e30f00ee9bfe10ff114c6ad
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-12-31 19:37:10 +0900

    [level unrated] Title: 짝수의 합, Time: 1.03 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
@@ -1,9 +1,9 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        for (int i = 2; i <= n; i += 2) {
-            answer += i;
-        }
-        return answer;
+        return IntStream.rangeClosed(0, n)
+            .filter(e -> e % 2 == 0)
+            .sum();
     }
 }

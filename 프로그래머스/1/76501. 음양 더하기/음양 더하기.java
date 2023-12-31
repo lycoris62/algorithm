7d2c60f121ddf381f4b8d899754028ff96a1c116
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-12-31 21:42:27 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -3,11 +3,7 @@ class Solution {
         int answer = 0;
         
         for (int i = 0; i < absolutes.length; i++) {
-            if (signs[i]) {
-                answer += absolutes[i];
-            } else {
-                answer -= absolutes[i];
-            }
+            answer += (signs[i] ? 1 : -1) * absolutes[i];
         }
         
         return answer;

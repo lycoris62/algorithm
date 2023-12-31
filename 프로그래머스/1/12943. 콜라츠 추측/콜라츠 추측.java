86cca6810d210d21226b2e8368f594ad3fb768fe
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-12-31 21:38:09 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,17 +1,12 @@
 class Solution {
     public int solution(long num) {
-        if (num == 1) return 0;
-        
-        int answer = 0;
-        
-        while (true) {
-            answer++;
-            if (answer > 500) return -1;
-            
-            if (num % 2 == 0) num /= 2;
-            else num = num * 3 + 1;
-            
-            if (num == 1) return answer;
+        long n = (long) num;
+        for( int i = 0; i < 500; i++){
+            if (n == 1) 
+                return i; 
+            n = (n % 2 == 0) ? n / 2 : n * 3 + 1;
         }
+
+        return -1;
     }
 }

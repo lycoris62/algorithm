67c8c3addf0976d94efbb28728ede31dc0f58c0a
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-12-31 19:56:12 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,10 +1,13 @@
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        char[] chars = String.valueOf(n).toCharArray();
         
-        for (char ch : chars) {
-            answer += ch - '0';
+        while (true) {
+            answer += n % 10;
+            
+            if (n < 10) break;
+            
+            n /= 10;
         }
         
         return answer;

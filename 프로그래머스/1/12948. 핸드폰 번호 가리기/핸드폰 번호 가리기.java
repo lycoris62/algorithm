48a77c69de795e53fe4d9108545c48227a676a53
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-12-31 21:44:11 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.41 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,8 +1,5 @@
 class Solution {
     public String solution(String phone_number) {
-        int len = phone_number.length();
-        String answer = phone_number.substring(len - 4);
-        
-        return "*".repeat(len - 4) + answer;
+        return phone_number.replaceAll(".(?=.{4})", "*");
     }
 }

55449870b2d657492acf25058860439bdc63bb7e
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2023-12-31 21:27:36 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.05 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,8 +1,9 @@
 class Solution {
     public long solution(long n) {
-        long sqrt = (long)Math.sqrt(n);
-        return (sqrt * sqrt == n)
-            ? (sqrt + 1) * (sqrt + 1)
-            : -1;
+        if (Math.pow((int) Math.sqrt(n), 2) == n) {
+            return (long) Math.pow(Math.sqrt(n) + 1, 2);
+        }
+
+        return -1;
     }
 }

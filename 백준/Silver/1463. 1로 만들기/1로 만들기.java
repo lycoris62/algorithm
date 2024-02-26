0ed7df9c53bd3dcefc2f39b68a14415f887e1f61
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-02-26 15:20:39 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 18128 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -11,23 +11,23 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = stoi(st.nextToken());
-        dp = new int[N + 1];
+        dp = new int[Math.max(N + 1, 4)];
+        dp[2] = dp[3] = 1;
 
-        for (int i = 2; i <= N; i++) {
-            dp[i] = Integer.MAX_VALUE;
-        }
+        for (int i = 4; i <= N; i++) {
+            int min = dp[i - 1];
 
-        for (int i = 1; i < N; i++) {
-            if (i * 3 <= N) {
-                dp[i * 3] = Math.min(dp[i] + 1, dp[i * 3]);
+            if (i % 2 == 0) {
+                min = Math.min(min, dp[i / 2]);
             }
-            if (i * 2 <= N) {
-                dp[i * 2] = Math.min(dp[i] + 1, dp[i * 2]);
+            if (i % 3 == 0) {
+                min = Math.min(min, dp[i / 3]);
             }
-            dp[i + 1] = Math.min(dp[i] + 1, dp[i + 1]);
+
+            dp[i] = min + 1;
         }
 
         System.out.println(dp[N]);

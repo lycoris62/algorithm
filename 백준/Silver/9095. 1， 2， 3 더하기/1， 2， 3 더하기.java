23d17f932c565b091c3c7a3c2ed47b3ee6f573c0
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-02-27 13:47:05 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,29 +1,37 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
+    static int T, N;
+    static int[] dp;
+    static StringBuilder sb = new StringBuilder();
+
     static int stoi(String s) {
         return Integer.parseInt(s);
     }
 
-    static int N;
-    static int T;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         T = stoi(st.nextToken());
+
         while (T-- > 0) {
             N = stoi(br.readLine());
-            System.out.println(dp(N));
-        }
-    }
+            dp = new int[Math.max(N + 1, 4)];
 
-    static int dp(int n) {
-        if (n == 1) return 1;
-        else if (n == 2) return 2;
-        else if (n == 3) return 4;
-        else return dp(n - 1) + dp(n - 2) + dp(n - 3);
+            dp[1] = 1; // 1
+            dp[2] = 2; // 1 + 1, 2
+            dp[3] = 4; // 1 + 1 + 1, 1 + 2, 2 + 1, 3 (순서에 따라 다름)
+
+            for (int i = 4; i <= N; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            }
+
+            sb.append(dp[N]).append("\n");
+        }
+
+        System.out.println(sb);
     }
 }

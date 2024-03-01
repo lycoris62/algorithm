5413b6bab7a9c22d67ae7ed696db2655bc001dce
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-03-01 23:38:32 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = stoi(st.nextToken());
+        M = stoi(st.nextToken());
+
+        System.out.println(N * M - 1);
+    }
+}
+

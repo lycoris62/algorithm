233d88eb50ac4a55f37e8c93c4ee92471bc8929d
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-03-17 12:52:19 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,27 +1,28 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int d = gcd(a, b);
 
-            System.out.println(d);
-            System.out.println(lcm(a, b, d));
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(gcd(Math.max(a, b), Math.min(a, b)));
+        System.out.println(lcm(Math.max(a, b), Math.min(a, b)));
     }
 
     static int gcd(int a, int b) {
         while (b != 0) {
-            int r = a % b;
-            a = b;
-            b = r;
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
+
         return a;
     }
 
-    static int lcm(int a, int b, int d) {
-        return a * b / d;
+    static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
     }
 }

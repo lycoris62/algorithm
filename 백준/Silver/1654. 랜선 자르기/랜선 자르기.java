39a626addb5d103384073b5c718cdbe67f372088
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-03-17 16:59:42 +0900

    [Silver II] Title: 랜선 자르기, Time: 204 ms, Memory: 17600 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,40 +1,48 @@
-import java.util.Scanner;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
- 
-		int K = in.nextInt();
-		int N = in.nextInt();
- 
-		int[] arr = new int[K];
-		
-		long max = 0;
-		for (int i = 0; i < K; i++) {
-			arr[i] = in.nextInt();
-			if(max < arr[i]) {
-				max = arr[i];
-			}
-		}
-		max++; 
- 
-		long min = 0; 
-		long mid = 0; 
- 
-		while (min < max) { 
-			mid = (max + min) / 2;
-			long count = 0;
-			for (int i = 0; i < arr.length; i++) {
-				count += (arr[i] / mid);
-			}
-			if(count < N) {
-				max = mid;
-			}
-			else {
-				min = mid + 1;
-			}
-		}
-		System.out.println(min - 1);
-	}
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int N, K;
+    static int[] lines;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        K = stoi(st.nextToken());
+        N = stoi(st.nextToken());
+
+        lines = new int[K];
+        for (int i = 0; i < K; i++) {
+            lines[i] = stoi(br.readLine());
+        }
+
+        long left = 1;
+        long right = Arrays.stream(lines).max().getAsInt();
+
+        // upper bound
+        while (left <= right) {
+            long mid = (left + right) >>> 1;
+
+            long count = 0;
+            for (int line : lines) {
+                count += line / mid;
+            }
+
+            // N보다 크면, 더 큼직하게 잘라도 되므로 왼쪽 버리기
+            // N와 같으면, N와 같은 횟수 중에서 더 큼직하게 잘라도 되는 길이 구하기
+            if (count >= N) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(left - 1); // N를 초과하는 첫 번째 값이므로, -1 해줘서 최댓값 구함
+    }
 }

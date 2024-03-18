75c28277c78f6962a71a731e6edaec94049826d5
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-03-18 14:52:17 +0900

    [Silver II] Title: 최소 힙, Time: 316 ms, Memory: 26324 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int N;
+    static Queue<Integer> pq = new PriorityQueue<>();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = stoi(st.nextToken());
+        while (N-- > 0) {
+            int x = stoi(br.readLine());
+            if (x != 0) {
+                pq.add(x);
+                continue;
+            }
+
+            sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}

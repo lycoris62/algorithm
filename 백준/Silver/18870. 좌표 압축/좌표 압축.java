094431eced26505fd7c873083e8c67649873e18c
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-03-18 14:33:18 +0900

    [Silver II] Title: 좌표 압축, Time: 2100 ms, Memory: 265192 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,30 +1,48 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
+
     static int stoi(String s) {
         return Integer.parseInt(s);
     }
 
     static int N;
-    static List<Integer> li = new ArrayList<>();
+    static int[] nums, temp;
+    static Map<Integer, Integer> map = new HashMap<>();
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = stoi(st.nextToken());
-        st = new StringTokenizer(br.readLine(), " ");
-        while (st.hasMoreTokens()) li.add(stoi(st.nextToken()));
+        nums = new int[N];
+        temp = new int[N];
 
-        List<Integer> collect = li.stream().distinct().sorted().collect(Collectors.toList());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int x = stoi(st.nextToken());
+            nums[i] = x;
+            temp[i] = x;
+        }
 
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < collect.size(); i++) map.put(collect.get(i), i);
+        // 1. 정렬 O(NlogN)
+        Arrays.sort(temp);
 
-        for (int i = 0; i < N; i++) sb.append(map.get(li.get(i)) + " ");
+        // 2. 차례대로 Map 에다가 INCR O(N)
+        int count = 0;
+        for (int x : temp) {
+            if (!map.containsKey(x)) {
+                map.put(x, count++);
+            }
+        }
+
+        // 3. 원본 배열과 Map 이용해서 좌표압축
+        for (int num : nums) {
+            sb.append(map.get(num)).append(" ");
+        }
+
         System.out.println(sb);
     }
 }

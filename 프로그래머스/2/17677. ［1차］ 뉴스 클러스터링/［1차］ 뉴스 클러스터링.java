bb84ed8d6920d6f7b9941bef0f03d827009d1432
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-04-17 14:06:58 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.52 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -2,18 +2,11 @@ import java.util.*;
 
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 0;
         str1 = str1.toLowerCase();
         str2 = str2.toLowerCase();
         
-        // Map<String, Integer> 로 관리하기
-        // 처음에는 keys()로 A, B 비교하다가, 둘이 같은 거 나오면 교집합과 합집합 각각 최소 최대 넣어주기
-        // 말은 이렇게 했지만 어떻게 구현함?? 
-        
         Map<String, Integer> map1 = new HashMap<>();
         Map<String, Integer> map2 = new HashMap<>();
-        // boolean isAlpha = Character.isAlphabetic(str1.charAt(1));
-        // System.out.println(isAlpha);
         
         for (int i = 0; i < str1.length() - 1; i++) {
             String word = str1.substring(i, i + 2);
@@ -37,9 +30,6 @@ class Solution {
             map2.put(word, map2.getOrDefault(word, 0) + 1);
         }
         
-        System.out.println(map1);
-        System.out.println(map2);
-        
         if (map1.isEmpty() && map2.isEmpty()) {
             return 65536;
         }
@@ -47,13 +37,7 @@ class Solution {
         if (map1.isEmpty() || map2.isEmpty()) {
             return 0;
         }
-        
-        // 이중 key 포문으로 서로 겹치는 것 찾기 
-        // 겹치는 게 있으면 두 value 비교해서 최솟값은 교집합수, 최댓값은 합집합수에 더하기
-        
-        // 아 둘이 안 겹치는 것들은 따로 합집합수에 넣어야 됨 
 
-        
         int min = 0;
         int max = 0;
         
@@ -67,6 +51,7 @@ class Solution {
             if (!map2.containsKey(key1)) {
                 max += map1.get(key1);        
             }
+            
             for (String key2 : map2.keySet()) {
                 if (!key1.equals(key2)) {
                     continue;
@@ -79,9 +64,6 @@ class Solution {
                 max += Math.max(val1, val2);
             }
         }
-        
-        System.out.println(min);
-        System.out.println(max);
         
         return (int) Math.floor(min * 65536.0 / max);
     }

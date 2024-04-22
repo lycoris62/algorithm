ce1956c3c4db557649a936ed073d8b514bb856b2
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-04-22 20:46:26 +0900

    [Silver IV] Title: 수들의 합 2, Time: 148 ms, Memory: 14996 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -8,7 +8,7 @@ public class Main {
     }
 
     static int N, M, answer;
-    static int[] nums, sums;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,49 +17,43 @@ public class Main {
         N = stoi(st.nextToken());
         M = stoi(st.nextToken());
 
-        nums = new int[N];
-        sums = new int[N + 1];
+        arr = new int[N];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            nums[i] = stoi(st.nextToken());
+            arr[i] = stoi(st.nextToken());
         }
 
+        // 시간 제한 0.5초, N <= 1만. O(N^2) -> 1억으로 시간 초과
+        // O(N)의 투 포인터로 풀이.
+
+        // 누적합부터. -> 아님. 변수 하나면 됨.
+        // M이 되면 answer++
+        // M보다 작으면 right++
+        // M보다 크면 left--;
+
+        // 누적합 쓰면 N * 4byte 만큼 메모리 낭비.
+        // 변수 하나에 저장하면 4byte만 쓰면 됨.... 멍청한 자식
+        // 거기에 편의용 길이 1 추가 안 해도 됨...
+
+        int sum = arr[0];
         int left = 0;
         int right = 0;
-        int sum = nums[0];
-
-        // 수도코드
-        /*
-
-        왼쪽 포인터 <- 0
-        오른쪽 포인터 <- 0
-        합 <- nums[0]
-
-        while (왼쪽 포인터 < 배열 길이) {
-            if (합 == 조건) {
-                경우 1 추가 
-            }
-            
-            if (합 > 조건 || 오른쪽 포인터가 끝에 도달) {
-                왼쪽 값을 합에서 빼기
-                왼쪽 포인터 한 칸 이동
-            } else if (합 <= 조건) {
-                오른쪽 포인터 한 칸 이동
-                그 값을 합에 더하기
-            }
-        }
-         */
 
         while (left < N) {
             if (sum == M) {
                 answer++;
             }
-            
-            if (sum > M || right == N - 1) {
-                sum -= nums[left++];
-            } else if (sum <= M) {
-                sum += nums[++right];
+
+            if (right == N - 1) {
+                sum -= arr[left++];
+                continue;
+            }
+
+            if (sum > M) {
+                sum -= arr[left++];
+            } else {
+                sum += arr[++right];
             }
         }
 

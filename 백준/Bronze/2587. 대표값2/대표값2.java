fefc29aff2a848eb0564be86c49f0d88925d76c0
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-04-28 23:42:39 +0900

    [Bronze II] Title: 대표값2, Time: 208 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println((int) Arrays.stream(arr).average().getAsDouble());
+        System.out.println(arr[2]);
+    }
+}

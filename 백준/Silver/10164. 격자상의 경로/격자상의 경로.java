23ea7ccbe68273192c2b6a2f022794b620ad9d3b
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-05-01 22:19:36 +0900

    [Silver II] Title: 격자상의 경로, Time: 128 ms, Memory: 14328 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
@@ -7,10 +7,7 @@ public class Main {
         return Integer.parseInt(s);
     }
 
-    static int N, M, K, answer;
-    static int[] dx = {1, 0};
-    static int[] dy = {0, 1};
-    static int[][] graph;
+    static int N, M, K;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,36 +17,25 @@ public class Main {
         M = stoi(st.nextToken());
         K = stoi(st.nextToken());
 
-        graph = new int[N][M];
+        int[][] dp = new int[N + 1][M + 1];
+        dp[0][1] = 1;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
 
         if (K == 0) {
-            dfs(0, 0, N - 1, M - 1);
+            System.out.println(dp[N][M]);
         } else {
-            int mx = K / M;
-            int my = K % M - 1;
+            int ax = K / M + (K % M > 0 ? 1 : 0);
+            int ay = K - (ax - 1) * M;
 
-            dfs(0, 0, mx, my);
-            int temp = answer;
-            answer = 0;
-            dfs(mx, my, N - 1, M - 1);
-            answer *= temp;
-        }
-        System.out.println(answer);
-    }
+            int bx = N - ax + 1;
+            int by = M - ay + 1;
 
-    static void dfs(int x, int y, int fx, int fy) {
-        if (x == fx && y == fy) {
-            answer++;
-            return;
-        }
-
-        for (int i = 0; i < 2; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if ((0 <= nx && nx < N) && (0 <= ny && ny < M)) {
-                dfs(nx, ny, fx, fy);
-            }
+            System.out.println(dp[ax][ay] * dp[bx][by]);
         }
     }
 }

d28dcf7f5951ae3d8bd785a7bd639fe6c141604a
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-05-05 16:09:30 +0900

    [Gold IV] Title: 탈출, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -24,7 +24,6 @@ public class Main {
 
         graph = new char[R][C];
         visited = new boolean[R][C];
-        Node hedgehog = null;
 
         for (int i = 0; i < R; i++) {
             String row = br.readLine();
@@ -33,11 +32,11 @@ public class Main {
                 graph[i][j] = x;
 
                 if (x == 'S') {
-                    start = new Node(i, j, 0, 'S');
+                    start = new Node(i, j, 0);
                 } else if (x == '*') {
-                    q.add(new Node(i, j, 0, '*'));
+                    q.add(new Node(i, j, 0));
                 } else if (x == 'D') {
-                    end = new Node(i, j, 0, 'D');
+                    end = new Node(i, j, 0);
                 }
             }
         }
@@ -49,79 +48,58 @@ public class Main {
         System.out.println(ans == -1 ? "KAKTUS" : ans);
     }
 
+    // visited 가 있어야 하는 이유 알아냄
+    // - visited 가 없으면 큐에 넣어진 것이 고슴도치(S) 였을지라도,
+    // graph[now.x][now.y] 가 홍수가 지나갔다면 물(*)로 로직이 돌아감. 진짜 바보다..
+
     static int bfs() {
         while (!q.isEmpty()) {
             Node now = q.poll();
+            char who = graph[now.x][now.y];
 
             if (now.x == end.x && now.y == end.y) {
-                return now.cnt;
+                return now.time;
             }
 
-            if (now.who == '*') {
-                for (int i = 0; i < 4; i++) {
-                    int nx = now.x + dx[i];
-                    int ny = now.y + dy[i];
+            for (int i = 0; i < 4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
 
-                    if ((0 <= nx && nx < R) && (0 <= ny && ny < C) && (!visited[nx][ny])) {
-                        if (graph[nx][ny] == 'S' || graph[nx][ny] == '.') {
-                            visited[nx][ny] = true;
-                            graph[nx][ny] = now.who;
-                            q.add(new Node(nx, ny, 0, '*'));
-                        }
-                    }
+                if (!isAccessible(nx, ny)) {
+                    continue;
                 }
-            } else if (now.who == 'S') {
-                for (int i = 0; i < 4; i++) {
-                    int nx = now.x + dx[i];
-                    int ny = now.y + dy[i];
 
-                    if ((0 <= nx && nx < R) && (0 <= ny && ny < C) && (!visited[nx][ny])) {
-                        if (graph[nx][ny] == '.' || graph[nx][ny] == 'D') {
-                            visited[nx][ny] = true;
-                            graph[nx][ny] = now.who;
-                            q.add(new Node(nx, ny, now.cnt + 1, 'S'));
-                        }
-                    }
+                if (isWaterMovable(who, graph[nx][ny]) || isHedgehogMovable(who, graph[nx][ny])) {
+                    visited[nx][ny] = true;
+                    graph[nx][ny] = who;
+                    q.add(new Node(nx, ny, now.time + 1));
                 }
             }
-
-//            for (int i = 0; i < 4; i++) {
-//                int nx = now.x + dx[i];
-//                int ny = now.y + dy[i];
-//
-//                if ((0 <= nx && nx < R) && (0 <= ny && ny < C)) {
-//                    if (now.who == '*') {
-//                        if (graph[nx][ny] == 'S' || graph[nx][ny] == '.') {
-////                            graph[nx][ny] = '*';
-//                            q.add(new Node(nx, ny, 0, '*'));
-//                        }
-//                    } else if (now.who == 'S') {
-//                        if (graph[nx][ny] == '.' || graph[nx][ny] == 'D') {
-//                            q.add(new Node(nx, ny, now.cnt + 1, 'S'));
-//                        }
-//
-//                        for (char[] row : graph) {
-//                            System.out.println(row);
-//                        }
-//                        System.out.println();
-//                    }
-//                }
-//            }
         }
 
         return -1;
     }
 
+    private static boolean isHedgehogMovable(char who, char next) {
+        return who == 'S' && (next == '.' || next == 'D');
+    }
+
+    private static boolean isWaterMovable(char who, char next) {
+        return who == '*' && (next == 'S' || next == '.');
+    }
+
+    private static boolean isAccessible(int nx, int ny) {
+        return (0 <= nx && nx < R) && (0 <= ny && ny < C) && (!visited[nx][ny]);
+    }
+
     static class Node {
 
-        int x, y, cnt;
-        char who;
+        int x, y, time;
 
-        public Node(int x, int y, int cnt, char who) {
+        public Node(int x, int y, int time) {
             this.x = x;
             this.y = y;
-            this.cnt = cnt;
-            this.who = who;
+            this.time = time;
         }
     }
 }

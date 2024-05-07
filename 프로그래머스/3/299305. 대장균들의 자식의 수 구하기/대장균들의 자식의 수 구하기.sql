f22c70145c703a65b252d0a6460c755a066d12cb
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-05-07 22:19:48 +0900

    [level 3] Title: 대장균들의 자식의 수 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
@@ -0,0 +1,13 @@
+SELECT
+    A.ID,
+    COUNT(B.ID) CHILD_COUNT
+FROM
+    ECOLI_DATA A
+    LEFT JOIN 
+        ECOLI_DATA B
+    ON
+        A.ID = B.PARENT_ID
+GROUP BY
+    A.ID
+ORDER BY
+    A.ID ASC

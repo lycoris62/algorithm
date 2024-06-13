ead95add6d37fa22908f4776003e2cdc36bef5a5
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-06-13 21:33:03 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 176 ms, Memory: 17552 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
@@ -5,25 +5,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int start = sc.nextInt();
-        int end = sc.nextInt();
+        int A = sc.nextInt();
+        int B = sc.nextInt();
 
-        int[] sums = new int[end + 1];
+        int count = 0;
+        int total = 0;
 
-        int x = 1;
+        for (int i = 1; i <= B; i++) {
+            for (int j = 1; j <= i; j++) {
+                count++;
 
-        OUTER:
-        for (int i = 1; i <= end; ) {
-            for (int j = 0; j < x; j++) {
-                if (i > end) {
-                    break OUTER;
+                if (A <= count && count <= B) {
+                    total += i;
                 }
-                sums[i] = sums[i - 1] + x;
-                i++;
             }
-            x++;
         }
 
-        System.out.println(sums[end] - sums[start - 1]);
+        System.out.println(total);
     }
 }

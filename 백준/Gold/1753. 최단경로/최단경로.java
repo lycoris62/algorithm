64b9442866aa3fc8bca7398a03e54b388895a60b
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-07-12 18:15:20 +0900

    [Gold IV] Title: 최단경로, Time: 676 ms, Memory: 109368 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,39 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     static int stoi(String s) {
         return Integer.parseInt(s);
     }
 
     static int V, E, K;
+    static int INF = Integer.MAX_VALUE / 10;
+    static boolean[] visited;
     static int[] distances;
-    static int INF = Integer.MAX_VALUE / 2;
-    static StringBuilder sb = new StringBuilder();
     static List<List<Edge>> graph = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         V = stoi(st.nextToken());
         E = stoi(st.nextToken());
         K = stoi(br.readLine());
 
         distances = new int[V + 1];
-        Arrays.fill(distances, INF);
+        visited = new boolean[V + 1];
+
+        for (int i = 0; i <= V; i++) {
+            distances[i] = INF;
+        }
 
         for (int i = 0; i <= V; i++) {
             graph.add(new ArrayList<>());
         }
 
         for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            graph.get(stoi(st.nextToken()))
-                .add(new Edge(stoi(st.nextToken()), stoi(st.nextToken())));
+            st = new StringTokenizer(br.readLine());
+            int u = stoi(st.nextToken());
+            int v = stoi(st.nextToken());
+            int w = stoi(st.nextToken());
+
+            graph.get(u).add(new Edge(v, w));
         }
 
         dijkstra(K);
+
         for (int i = 1; i <= V; i++) {
             sb.append(distances[i] == INF ? "INF" : distances[i]).append("\n");
         }
@@ -43,42 +51,38 @@ public class Main {
     }
 
     static void dijkstra(int start) {
+        distances[start] = 0;
         Queue<Edge> pq = new PriorityQueue<>();
         pq.add(new Edge(start, 0));
-        distances[start] = 0;
 
         while (!pq.isEmpty()) {
-            Edge nowNode = pq.poll();
-            int now = nowNode.index;
-            int nowCost = nowNode.cost;
+            Edge now = pq.poll();
 
-            if (distances[now] < nowCost) {
+            if (visited[now.v]) {
                 continue;
             }
 
-            for (Edge edge : graph.get(now)) {
-                int cost = distances[now] + edge.cost;
-                if (cost < distances[edge.index]) {
-                    distances[edge.index] = cost;
-                    pq.add(new Edge(edge.index, cost));
+            visited[now.v] = true;
+
+            for (Edge next : graph.get(now.v)) {
+                if (distances[next.v] > now.w + next.w) {
+                    distances[next.v] = now.w + next.w;
+                    pq.add(new Edge(next.v, now.w + next.w));
                 }
             }
         }
     }
 
     static class Edge implements Comparable<Edge> {
+        int v, w;
 
-        int index, cost;
-
-        public Edge(int index, int cost) {
-            this.index = index;
-            this.cost = cost;
+        public Edge(int v, int w) {
+            this.v = v;
+            this.w = w;
         }
 
-        @Override
         public int compareTo(Edge o) {
-            return Integer.compare(this.cost, o.cost);
+            return Integer.compare(this.w, o.w);
         }
     }
 }
-

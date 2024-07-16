1df3cc80c9a8e95b533a9a230e75d50d8d331167
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-07-16 14:55:55 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1540 ms, Memory: 330324 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -6,50 +6,77 @@ public class Main {
         return Integer.parseInt(s);
     }
 
-    static int N, M, res = 0;
+    static int N, M, totalCost, maxCost;
     static int[] parents;
-    static List<int[]> edgeList = new ArrayList<>();
+    static List<Edge> edges = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = stoi(st.nextToken());
         M = stoi(st.nextToken());
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            edgeList.add(new int[]{stoi(st.nextToken()), stoi(st.nextToken()), stoi(st.nextToken())});
-        }
 
         parents = new int[N + 1];
         for (int i = 1; i <= N; i++) {
             parents[i] = i;
         }
 
-        edgeList.sort(Comparator.comparing(e -> e[2]));
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = stoi(st.nextToken());
+            int B = stoi(st.nextToken());
+            int C = stoi(st.nextToken());
 
-        int bigCost = 0;
-        for (int[] edge : edgeList) {
-            if (find(edge[0]) != find(edge[1])) {
-                res += edge[2];
-                union(edge[0], edge[1]);
-                bigCost = edge[2];
+            edges.add(new Edge(A, B, C));
+        }
+
+        Collections.sort(edges);
+
+        for (Edge edge : edges) {
+            if (union(edge.u, edge.v)) {
+                totalCost += edge.w;
+                maxCost = Math.max(maxCost, edge.w);
             }
         }
 
-        System.out.println(res - bigCost);
+        System.out.println(totalCost - maxCost);
     }
 
-    static int find(int n) {
-        if (n == parents[n]) return n;
-        return parents[n] = find(parents[n]);
-    }
-
-    static void union(int a, int b) {
+    static boolean union(int a, int b) {
         int aRoot = find(a);
         int bRoot = find(b);
-        if (aRoot != bRoot)
-            parents[bRoot] = aRoot;
+
+        if (aRoot == bRoot) {
+            return false;
+        }
+
+        int min = Math.min(aRoot, bRoot);
+        int max = Math.max(aRoot, bRoot);
+        parents[max] = min;
+
+        return true;
+    }
+
+    static int find(int x) {
+        if (parents[x] == x) {
+            return x;
+        }
+
+        return parents[x] = find(parents[x]);
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int u, v, w;
+
+        public Edge(int u, int v, int w) {
+            this.u = u;
+            this.v = v;
+            this.w = w;
+        }
+
+        public int compareTo(Edge o) {
+            return Integer.compare(this.w, o.w);
+        }
     }
 }

bcf5450f8766205d75f7dbbdf35b1639744db5c0
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-08-19 00:27:43 +0900

    [Bronze IV] Title: FA, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/14935. FA/FA.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String x = br.readLine();
+		System.out.println("FA");
+	}
+}

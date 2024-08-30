c6e1e0e6294223023a2be86615a7b85473c419cb
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-08-30 10:15:56 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 192 ms, Memory: 19432 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int T, N;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        T = stoi(st.nextToken());
+
+        while (T-- > 0) {
+            String word = br.readLine();
+            N = 0;
+            sb.append(isPalindrome(word)).append(" ").append(N).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static int recursion(String s, int l, int r) {
+        N++;
+        if (l >= r) return 1;
+        else if (s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l + 1, r - 1);
+    }
+
+    public static int isPalindrome(String s) {
+        return recursion(s, 0, s.length() - 1);
+    }
+}

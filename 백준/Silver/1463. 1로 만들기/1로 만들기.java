15c0b923e0162a45adf0a7dabc0c53c19a3b1b6f
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-10-04 22:58:19 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18148 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,34 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     static int stoi(String s) {
         return Integer.parseInt(s);
     }
 
     static int N;
-    static int[] dp;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = stoi(st.nextToken());
-        dp = new int[Math.max(N + 1, 4)];
-        dp[2] = dp[3] = 1;
+        arr = new int[Math.max(N, 3) + 1];
+        arr[2] = arr[3] = 1;
 
         for (int i = 4; i <= N; i++) {
-            int min = dp[i - 1];
+            int min = arr[i - 1];
 
             if (i % 2 == 0) {
-                min = Math.min(min, dp[i / 2]);
+                min = Math.min(min, arr[i / 2]);
             }
             if (i % 3 == 0) {
-                min = Math.min(min, dp[i / 3]);
+                min = Math.min(min, arr[i / 3]);
             }
 
-            dp[i] = min + 1;
+            arr[i] = min + 1;
         }
 
-        System.out.println(dp[N]);
+        System.out.println(arr[N]);
     }
 }

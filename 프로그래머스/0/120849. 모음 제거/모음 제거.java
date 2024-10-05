0d8516266f2a127ad1a731e03d209ccf0455d06d
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-10-05 09:31:54 +0900

    [level 0] Title: 모음 제거, Time: 0.12 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,15 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        StringBuilder sb = new StringBuilder();
-        
-        for (char ch : my_string.toCharArray()) {
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                continue;
-            }
-            
-            sb.append(ch);
-        }
-        
-        return sb.toString();
+        return my_string.replaceAll("[aeiou]", "");
     }
 }

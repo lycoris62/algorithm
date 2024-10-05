54688a15a41874e7f0869d25b78db41c6c2176d1
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-10-06 03:07:35 +0900

    [Silver I] Title: 회의실 배정, Time: 508 ms, Memory: 45144 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,30 +1,56 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static int N, count, prevTime;
+    static List<Meeting> meetings = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] times = new int[N][2];
-        
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            times[i][0] = Integer.parseInt(st.nextToken());
-            times[i][1] = Integer.parseInt(st.nextToken());
-        }
-        
-        Arrays.sort(times, Comparator.comparingInt((int[] arr) -> arr[1]).thenComparingInt((int[] arr) -> arr[0]));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int res = 1;
-        int lastEndTime = times[0][1];
-        for (int i = 1; i < N; i++) {
-            if (lastEndTime <= times[i][0]) {
-                res++;
-                lastEndTime = times[i][1];
+        N = stoi(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = stoi(st.nextToken());
+            int end = stoi(st.nextToken());
+
+            meetings.add(new Meeting(start, end));
+        }
+
+        Collections.sort(meetings);
+
+        for (Meeting meeting : meetings) {
+            if (prevTime <= meeting.start) {
+                prevTime = meeting.end;
+                count++;
             }
         }
-        System.out.println(res);
+
+        System.out.println(count);
+    }
+
+    static class Meeting implements Comparable<Meeting> {
+
+        int start, end;
+
+        public Meeting(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public int compareTo(Meeting o) {
+            if (this.end == o.end) {
+                return Integer.compare(this.start, o.start);
+            }
+
+            return Integer.compare(this.end, o.end);
+        }
     }
 }

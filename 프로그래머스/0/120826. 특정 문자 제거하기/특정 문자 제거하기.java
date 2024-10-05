cacba15a7f76f19758025cba345f3b564ce2b4fc
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-10-05 12:21:54 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.05 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,5 +1,5 @@
 class Solution {
     public String solution(String my_string, String letter) {
-        return my_string.replaceAll("[" + letter + "]", "");
+        return my_string.replace(letter, "");
     }
 }

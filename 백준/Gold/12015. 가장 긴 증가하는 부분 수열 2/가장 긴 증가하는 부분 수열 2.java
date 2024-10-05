afb89bf37f0a16310dcf6f473e9320317875cb5b
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-10-06 02:27:46 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 664 ms, Memory: 121496 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -2,48 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     static int stoi(String s) {
         return Integer.parseInt(s);
     }
 
-    static int N, length = 1;
-    static int[] arr, list;
+    static int N;
+    static int[] arr;
+    static List<Integer> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = stoi(st.nextToken());
-        arr = new int[N + 1];
-        list = new int[N + 1];
 
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 1; i <= N; i++) {
+        st = new StringTokenizer(br.readLine());
+        arr = new int[N];
+        list.add(0);
+
+        for (int i = 0; i < N; i++) {
             arr[i] = stoi(st.nextToken());
-        }
 
-        list[1] = arr[1];
-        for (int i = 2; i <= N; i++) {
-            if (list[length] < arr[i]) {
-                list[length + 1] = arr[i];
-                length++;
-            } else {
-                list[binarySearch(arr[i], 1, length)] = arr[i];
+            if (arr[i] > list.get(list.size() - 1)) {
+                list.add(arr[i]);
+                continue;
             }
-        }
 
-        System.out.println(length);
-    }
+            int left = 0;
+            int right = list.size() - 1;
 
-    static int binarySearch(int target, int left, int right) {
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (list[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid;
+            while (left < right) {
+                int mid = (left + right) / 2;
+
+                if (list.get(mid) >= arr[i]) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
             }
+
+            list.set(right, arr[i]);
         }
-        return right;
+
+        System.out.println(list.size() - 1);
     }
 }

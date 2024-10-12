078594e6233614fbb1d9eb9e2af7d0a1599071af
Lycoris62 <55584664+lycoris62@users.noreply.github.com>
2024-10-12 15:53:26 +0900

    [Bronze III] Title: 다면체, Time: 96 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int V = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
+			
+			System.out.println(E - V + 2);
+		}
+	}
+}

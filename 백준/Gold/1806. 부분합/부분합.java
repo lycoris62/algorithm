1681f66e9d6535dd8d0c3931b41453ce5e66441e
김재윤 <55584664+yunjae62@users.noreply.github.com>
2024-11-27 18:15:35 +0900

    [Gold IV] Title: 부분합, Time: 264 ms, Memory: 24684 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,48 +1,56 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    static int N, S, min = Integer.MAX_VALUE;
+    static int[] arr, sums;
+
     static int stoi(String s) {
         return Integer.parseInt(s);
     }
 
-    static int N, S, left = 0, right = 0, res = Integer.MAX_VALUE;
-    static int[] arr, sums;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = stoi(st.nextToken());
         S = stoi(st.nextToken());
+
         arr = new int[N];
         sums = new int[N];
 
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = stoi(st.nextToken());
+            int input = stoi(st.nextToken());
+            arr[i] = input;
+            sums[i] = input;
         }
 
-        sums[0] = arr[0];
         for (int i = 1; i < N; i++) {
-            sums[i] = sums[i - 1] + arr[i];
+            sums[i] += sums[i - 1];
         }
 
-        while (left <= right) {
-            int sum = sums[right] - sums[left] + arr[left];
+        int end = 0;
 
-            if (sum >= S) {
-                res = Math.min(res, right - left + 1);
-                left++;
-            } else {
-                if (right == arr.length - 1) {
-                    left++;
-                } else {
-                    right++;
-                }
+        for (int start = 0; start < N; start++) {
+            while (end < N && sums[end] - sums[start] + arr[start] < S) {
+                end++;
+            }
+
+            if (end == N) {
+                break;
+            }
+
+            int total = sums[end] - sums[start] + arr[start];
+
+            if (total >= S) {
+                min = Math.min(end - start + 1, min);
             }
         }
 
-        System.out.println(res == Integer.MAX_VALUE ? 0 : res);
+        System.out.println(min == Integer.MAX_VALUE ? 0 : min);
     }
 }

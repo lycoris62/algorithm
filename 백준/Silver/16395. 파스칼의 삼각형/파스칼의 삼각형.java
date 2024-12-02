217ae15a5c39cea6a0479711c98d2cca005a2d7e
김재윤 <55584664+yunjae62@users.noreply.github.com>
2024-12-03 01:40:29 +0900

    [Silver V] Title: 파스칼의 삼각형, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, K;
+    static int[][] arr;
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = stoi(st.nextToken());
+        K = stoi(st.nextToken());
+
+        arr = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i][0] = 1;
+            for (int j = 1; j < i; j++) {
+                arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+            }
+            arr[i][i] = 1;
+        }
+
+        System.out.println(arr[N - 1][K - 1]);
+    }
+}

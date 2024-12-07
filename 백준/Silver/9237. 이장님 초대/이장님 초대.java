4228abe0908faa461200d7410e55574144ae6342
김재윤 <55584664+yunjae62@users.noreply.github.com>
2024-12-07 22:17:14 +0900

    [Silver V] Title: 이장님 초대, Time: 320 ms, Memory: 27908 KB -BaekjoonHub

---
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, max;
+    static int[] arr;
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = stoi(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = stoi(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            max = Math.max(max, arr[i] - i);
+        }
+
+        System.out.println(max + N + 1);
+    }
+}

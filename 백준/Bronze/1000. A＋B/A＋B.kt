e3811b2eb13e99344506c0f1a30c0344202ad9c5
김재윤 <55584664+yunjae62@users.noreply.github.com>
2024-12-13 02:50:32 +0900

    [Bronze V] Title: A+B, Time: 80 ms, Memory: 12196 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.kt
+++ 백준/Bronze/1000. A＋B/A＋B.kt
@@ -1,5 +1,9 @@
-import java.util.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
 
-fun main() = with(Scanner(System.`in`)) {
-    println(nextInt() + nextInt())
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val st = StringTokenizer(br.readLine())
+    println(st.nextToken().toInt() + st.nextToken().toInt())
 }
